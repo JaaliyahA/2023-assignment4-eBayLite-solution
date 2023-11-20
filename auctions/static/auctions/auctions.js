f5bcbc49610461a21ea09a87c84f3dd079c5c2e3
Jaaliyah <jaaliyah_alexander@outlook.com>
2023-11-19 23:08:20 -0900

    new branch

--- auctions/static/auctions/auctions.js
+++ auctions/static/auctions/auctions.js
@@ -1,8 +1,10 @@
+
+
 // const wlButton = document.getElementById('watchlistButton').value
 const csrftoken = getCookie('csrftoken');
 // // const bdButton = document.querySelector('#bidButton')
 // // const comButton = document.querySelector('#commentButton')
-const url = window.location.href
+
 
 function getCookie(name) {
     let cookieValue = null;
@@ -21,44 +23,130 @@ function getCookie(name) {
 }
 
 
-function handleWatchlist(form) {
-    console.log(form)
+function handleWatchlist(id, url) {
     let body = {
         "doit": "toggle-watcher"
     }
     const sending = JSON.stringify(body)
+    //console.log(sending)
     const options = {
         method: 'POST',
-        headers: { 'X-CSRFToken': csrftoken },
+        headers: {
+            'X-CSRFToken': csrftoken,
+            'Content-Type': 'application/json'
+        },
         mode: 'same-origin',
-        body: sending
+        body: JSON.stringify(body)
     }
+    console.log(options.body)
     fetch(url, options)
-        .then(response => console.log(response))
+        .then(response => response.json())
         .then(data => {
             console.log(data)
+            if (data['watching']) {
+                document.querySelector(`[value="${id}"]`).innerHTML = `<i class="bi bi-heart-fill"></i>`
+            } else {
+                document.querySelector(`[value="${id}"]`).innerHTML = `<i class="bi bi-heart"></i>`
+            }
         })
     // let watch = document.querySelector('#watchlistButton').value
     // watch = !watch;
     // console.log(watch)
 
 }
+function handleComments(id, form) {
+    const url = window.location.href
+    let formInp = {}
+
+    for (const [key, value] of form) {
+        console.log(`${key}: ${value}`);
+        formInp[key] = value
+    }
+    let body = {
+        "doit": "add-comment",
+        'form': formInp
+    }
+    const options = {
+        method: 'POST',
+        headers: {
+            'X-CSRFToken': csrftoken,
+            'Content-Type': 'application/json'
+        },
+        mode: 'same-origin',
+        body: JSON.stringify(body)
+    }
+    console.log(options.body)
+    fetch(url, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok')
+            }
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+            if (data['success']) {
+                document.querySelector('#commForm').classList.add("d-none")
+                document.getElementById('commentButton').classList.remove("d-none")
+            } else {
+                alert('yet another error')
+            }
+
+        })
+        .catch(error => {
+            console.error('Problem with Fetch')
+        })
+}
+
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#watchlistForm').addEventListener("submit", e => {
-        const wlButton = document.getElementById('watchlistButton').value
-        //const csrftoken = getCookie('csrftoken');
-        //const bdButton = document.querySelector('#bidButton').value
-        //const comButton = document.querySelector('#commentButton').value
-        e.preventDefault();
-        console.log(wlButton)
-        const formData = new FormData(document.querySelector('#watchlistForm'))
-        handleWatchlist(formData);
-    })
+    const wlButton = document.querySelectorAll('#watchlistButton')
+    const listingWatchlist = document.querySelector('#watchlistForm')
 
+    if (listingWatchlist) {
+        const watchlist = document.querySelector('#watchlistButton')
+        const comment = document.getElementById('commentButton')
 
+        if (wlButton.length != 0 && !listingWatchlist) {
+            wlButton.forEach(link => link.addEventListener('click', e => {
+                let id = link.value
+                let url = `listing/${id}`
+                handleWatchlist(id, url);
+            }))
+
+        }
+
+        if (watchlist && comment) {
+            watchlist.addEventListener('click', e => {
+                e.preventDefault();
+                const url = window.location.href
+                handleWatchlist(watchlist.value, url)
+            })
+
+            comment.addEventListener('click', e => {
+                e.preventDefault();
+                document.querySelector('#commForm').classList.remove("d-none")
+                comment.classList.add("d-none")
+                let id = comment.value
+                const textarea = document.querySelector('#id_comment')
+
+                if (textarea) {
+                    document.getElementById('addCommForm').addEventListener('submit', e => {
+                        e.preventDefault();
+                        console.log("do we get here")
+                        const form = document.getElementById("addCommForm")
+                        console.log(form)
+                        const f = new FormData(form)
+                        console.log(f)
+
+                        handleComments(id, f)
+                    })
+                }
+            })
+        }
+    }
 })
 
 
